69e7201ceab943cc55b1df36359b5231128e8a47
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-05-24 13:13:46 +0200

    general layout is set

---
+++ multimedia_frontend/lib/Components/VideoComponent.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class VideoRetrivalComponent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('this is video retrival component'),
+    );
+  }
+}
--- multimedia_frontend/lib/screens/SearchScreen.dart
+++ multimedia_frontend/lib/screens/SearchScreen.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:multimedia_frontend/Components/ImageCompnent.dart';
+import 'package:multimedia_frontend/Components/VideoComponent.dart';
 
 class SearchScreen extends StatelessWidget {
   static const routeName = 'SEARCH_SCREEN';
 
   @override
   Widget build(BuildContext context) {
-  final args = ModalRoute.of(context)!.settings.arguments as String;
-
-    return Container(
-      child: Text('this is the ${args} screen'),
-    );
+    final args = ModalRoute.of(context)!.settings.arguments as String;
+    Widget renderElement = VideoRetrivalComponent();
+    if (args == 'Image') {
+      renderElement = ImageRetrivalComponent();
+    }
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Content Based $args Retrival System'),
+        ),
+        body: Column(
+          children: [
+            renderElement,
+            Expanded(
+              child: Center(child: Text('result area')),
+            )
+          ],
+        ));
   }
 }
