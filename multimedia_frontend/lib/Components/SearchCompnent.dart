f08bb41581c8bc5f1b714fb5e782f993bff9623a
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-05-27 07:17:24 +0200

    Search component finished

---
+++ multimedia_frontend/lib/Components/SearchCompnent.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class SearchComponent extends StatelessWidget {
+  final String searchType;
+  SearchComponent({this.searchType='error'});
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(), hintText: '${searchType} URL'),
+                    ),
+                  ),
+                  IconButton(
+                      onPressed: () => {},
+                      icon: FaIcon(FontAwesomeIcons.search))
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton.icon(
+                onPressed: () => {},
+                icon: FaIcon(FontAwesomeIcons.handsHelping),
+                label: Text('Helping $searchType')),
+          )
+        ],
+      ),
+    );
+  }
+}
--- multimedia_frontend/lib/screens/SearchScreen.dart
+++ multimedia_frontend/lib/screens/SearchScreen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:multimedia_frontend/Components/ImageCompnent.dart';
-import 'package:multimedia_frontend/Components/VideoComponent.dart';
+import 'package:multimedia_frontend/Components/SearchCompnent.dart';
+
 
 class SearchScreen extends StatelessWidget {
   static const routeName = 'SEARCH_SCREEN';
@@ -8,17 +8,14 @@ class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final args = ModalRoute.of(context)!.settings.arguments as String;
-    Widget renderElement = VideoRetrivalComponent();
-    if (args == 'Image') {
-      renderElement = ImageRetrivalComponent();
-    }
+   
     return Scaffold(
         appBar: AppBar(
           title: Text('Content Based $args Retrival System'),
         ),
         body: Column(
           children: [
-            renderElement,
+            SearchComponent(searchType: args,),
             Expanded(
               child: Center(child: Text('result area')),
             )
