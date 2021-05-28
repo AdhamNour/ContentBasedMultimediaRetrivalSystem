865ad0ef8897bb3f58b59cc91bb970fc62d6316f
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2021-05-28 14:48:43 +0200

    finished choosing the algorithm for the image

--- multimedia_frontend/lib/Components/SearchCompnent.dart
+++ multimedia_frontend/lib/Components/SearchCompnent.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class SearchComponent extends StatelessWidget {
+class SearchComponent extends StatefulWidget {
   final String searchType;
-  SearchComponent({this.searchType='error'});
+  SearchComponent({this.searchType = 'error'});
+
+  @override
+  _SearchComponentState createState() => _SearchComponentState();
+}
+
+class _SearchComponentState extends State<SearchComponent> {
+  var algorithms = {'Histogram': false, 'Text': true};
   @override
   Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
     return Card(
       child: Column(
         children: [
@@ -18,7 +26,8 @@ class SearchComponent extends StatelessWidget {
                   Expanded(
                     child: TextField(
                       decoration: InputDecoration(
-                          border: OutlineInputBorder(), hintText: '${searchType} URL'),
+                          border: OutlineInputBorder(),
+                          hintText: '${widget.searchType} URL'),
                     ),
                   ),
                   IconButton(
@@ -34,8 +43,30 @@ class SearchComponent extends StatelessWidget {
             child: ElevatedButton.icon(
                 onPressed: () => {},
                 icon: FaIcon(FontAwesomeIcons.handsHelping),
-                label: Text('Helping $searchType')),
-          )
+                label: Text('Helping ${widget.searchType}')),
+          ),
+          if (widget.searchType == 'Image')
+            Padding(
+              //Add Camera Button
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: ListView(
+                  children: algorithms.keys
+                      .map((e) => CheckboxListTile(
+                          value: algorithms[e],
+                          onChanged: (newVal) {
+                            setState(() {
+                              if (newVal != null) {
+                                algorithms[e] = newVal ? true : false;
+                              }
+                            });
+                          },
+                          title: Text(e)))
+                      .toList(),
+                ),
+                height: screenSize.height*0.15,
+              ),
+            )
         ],
       ),
     );
